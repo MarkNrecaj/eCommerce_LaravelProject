2eeb1f78f3c48db9aebb68d378bccc789c0c3d15
Krenar <Krenar@users.noreply.github.com>
2020-06-29 15:34:25 +0200

    bug fixed

--- resources/views/postal_worker_orders.blade.php
+++ resources/views/postal_worker_orders.blade.php
@@ -22,67 +22,45 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    @if (count($orders) <= 0)
-                        <h3>No orders assigned for you yet.</h3>
-                    @else
-                        <div class="card table-responsive">
 
                     <div class="card table-responsive" >
                             <table class="table">
                                 <thead class="thead-light">
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Receiver</th>
-                                        <th scope="col">Tel</th>
-                                        <th scope="col">Full Address</th>
-                                        <th scope="col">Quantity</th>
-                                        <th scope="col">Weight</th>
-                                        <th scope="col">Order Type</th>
-                                        <th scope="col">Openable</th>
-                                        <th scope="col">Returnable</th>
-                                        <th scope="col">Additional Notes</th>
-                                        <th scope="col">Order Name</th>
-                                        <th scope="col">Description</th>
-                                        <th scope="col">Price</th>
-                                        <th scope="col">Total Price</th>
-                                        <th scope="col">Status</th>
-                                    </tr>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Receiver</th>
+                                    <th scope="col">Tel</th>
+                                    <th scope="col">Full Address</th>
+                                    <th scope="col">Quantity</th>
+                                    <th scope="col">Weight</th>
+                                    <th scope="col">Order Type</th>
+                                    <th scope="col">Openable</th>
+                                    <th scope="col">Returnable</th>
+                                    <th scope="col">Additional Notes</th>
+                                    <th scope="col">Order Name</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Price</th>
+                                    <th scope="col">Total Price</th>
+                                    <th scope="col">Status</th>
+                                </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach($orders as $key =>$order)
-                                        <tr>
-                                            <td>{{$key+1}}</td>
-                                            <td>{{$order->receiver_name}}</td>
-                                            <td>@if($order->receiver_tel2 == null)
-                                                    {{$order->receiver_tel}}
-                                                @else
-                                                    {{$order->receiver_tel. " ose " .$order->receiver_tel2}}
-                                                @endif
+                                @foreach($orders as $key =>$order)
+                                    <tr>
+                                        <td>{{$key+1}}</td>
+                                        <td>{{$order->receiver_name}}</td>
+                                        <td>@if($order->receiver_tel2 == null)
+                                                {{$order->receiver_tel}}
+                                            @else
+                                                {{$order->receiver_tel. " ose " .$order->receiver_tel2}}
+                                            @endif
+                                        </td>
+                                        <td>@if($order->address == null)
+                                                {{$order->state. ', '.$order->city}}
+                                            @else
+                                                {{$order->state. ', '.$order->city. ', ' .$order->address}}
+                                            @endif
                                             </td>
-                                            <td>@if($order->address == null)
-                                                    {{$order->state. ', '.$order->city}}
-                                                @else
-                                                    {{$order->state. ', '.$order->city. ', ' .$order->address}}
-                                                @endif
-                                                </td>
-                                            <td>{{$order->quantity}}</td>
-                                            <td>{{$order->weight}}</td>
-                                            <td>{{$order->order_type}}</td>
-                                            <td>@if($order->is_openable) YES
-                                                @else No
-                                                @endif
-                                            </td>
-                                            <td>@if($order->is_returnable) YES
-                                                @else No
-                                                @endif</td>
-                                            <td>{{$order->additional_notes}}</td>
-                                            <td>{{$order->order_name}}</td>
-                                            <td>{{$order->description}}</td>
-                                            <td>{{$order->price}}</td>
-                                            <td>{{$order->total_price}}</td>
-                                            <td>{{$order->status}}</td>
-                                        </tr>
-                                    @endforeach
                                         <td>{{$order->quantity}}</td>
                                         <td>{{$order->weight}}</td>
                                         <td>{{$order->order_type}}</td>
@@ -112,7 +90,6 @@
                                 </tbody>
                             </table>
                         </div>
-                    @endif
                 </div>
             </div>
         </div>
