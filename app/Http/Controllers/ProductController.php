d89c9f72028b26a72ac928af91a03478acb8b6ab
Krenar <Krenar@users.noreply.github.com>
2020-07-14 12:36:12 +0200

    Added phone number field on product tracking page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -105,12 +105,13 @@ class ProductController extends Controller
         $this->validate(
             $request,
             [
-                'tracking_id' => 'numeric',
-                'buyerEmail' => 'email'
+                'tracking_id' => 'required|numeric',
+                'buyerPhone' => 'required|numeric'
             ]
         );
 
-        $order = Order::find($request->tracking_id);
+        $order = Order::where('id', $request['tracking_id'])
+            ->where('receiver_tel', $request['buyerPhone'])->first();
         //dd($order);
         $msg = $order == null ? 'Sorry , We couldn\'t find your order' : 'We found your order, ' . $order->receiver_name;
         $status = $order == null ? 'Please make sure you correctly entered tracking ID. Otherwise, try again in a while.' : 'You order status is: ' . $order->status;
--- resources/views/track/view.blade.php
+++ resources/views/track/view.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 <div class="text-center py-5">
-    <h1 >Enter email and tracking id</h1>
+    <h1 >Enter phone number and tracking id</h1>
     <form action="{{route('order.track')}}" method="post">
         @csrf
         <br> 
-        <input type="email" placeholder = "Email" name="buyerEmail" class="form-control" style="width: 300px; display: block; margin : 0 auto;">
+        <input type="number" min="0" placeholder = "Phone number without +" name="buyerPhone" class="form-control" style="width: 300px; display: block; margin : 0 auto;">
         <br>
-        <input type="text" placeholder = "Tracking ID" name="tracking_id" id="tracking_id" class="form-control" style="width: 300px; display: block; margin : 0 auto;">
+        <input type="number" min="0" placeholder = "Tracking ID" name="tracking_id" id="tracking_id" class="form-control" style="width: 300px; display: block; margin : 0 auto;">
         <br>
         <button type="submit" class="btn btn-outline-primary">Search</button>
     </form>
