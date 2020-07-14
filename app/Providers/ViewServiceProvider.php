36eeaea636eef42b213b1519cc54de3844dd6cfc
djellzebllaca <djellzebllaca@gmail.com>
2020-07-14 12:00:10 +0200

    Checking if user is logged in.

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -27,15 +27,19 @@ class ViewServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Using Closure based composers...
-        View::composer('*', function ($view) {
-            $cart = Cart::where('buyer_id', Auth::user()->id)->get();
-            $products = [];
-            foreach ($cart as $item) {
-                array_push($products, Product::find($item->product_id));
-            }
-            $cartItems = count($products);
-            $view->with('cartItems', $cartItems );
-        });
+            // Using Closure based composers...
+            View::composer('layouts/app', function ($view) {
+
+                if (Auth::check()) {
+                $cart = Cart::where('buyer_id', Auth::user()->id)->get();
+                $products = [];
+                foreach ($cart as $item) {
+                    array_push($products, Product::find($item->product_id));
+                }
+                $cartItems = count($products);
+                $view->with('cartItems', $cartItems );
+                }
+            });
+
     }
 }
